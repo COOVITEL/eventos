a040a4541c8971cd4325e7c9e7877b538f03c069
ManuRodriguezC <estebanclimb@gmail.com>
2025-01-14 09:44:48 -0500

    Add tailwind and consultas app

--- eventos/settings.py
+++ eventos/settings.py
@@ -37,8 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
+    'tailwind',
+    'theme',
+    'django_browser_reload',
+    'consultas'
 ]
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'eventos.urls'
---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,27 @@
+/**
+ * This is a minimal config.
+ *
+ * If you need the full config, get it from here:
+ * https://unpkg.com/browse/tailwindcss@latest/stubs/defaultConfig.stub.js
+ */
+
+module.exports = {
+    content: [
+        '../templates/**/*.html',
+        '../../templates/**/*.html',
+        '../../**/templates/**/*.html',
+    ],
+    theme: {
+        extend: {
+            backgroundColor : {
+                'blueCoovitel': '#262D63'
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
